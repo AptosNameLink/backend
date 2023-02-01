524b998e188b57b121e2b2467daf70a4bea4f83f
Sigrid <sigrid.jinhyung@gmail.com>
2023-02-02 00:56:47 +0900

    feat: upload to ipfs

--- src/package/aptos.rs
+++ src/package/aptos.rs
@@ -1,8 +1,29 @@
+use std::{env, future};
+use std::io::Cursor;
 use aptos_sdk::crypto::ed25519::{Ed25519PublicKey, Ed25519Signature};
 use aptos_sdk::crypto::{Signature, ValidCryptoMaterialStringExt};
 use aptos_sdk::types::transaction::TransactionArgument::Address;
 use ed25519_dalek::Signature as DalekSignature;
 use std::str::FromStr;
+use actix_web::http::Uri;
+use serde::{Deserialize, Serialize};
+use actix_multipart::Multipart;
+use actix_web::http;
+use actix_web::web::Bytes;
+use actix_web::http::header::HeaderMap;
+use serde_json::from_str;
+// use ipfs_api_backend_actix::{IpfsApi, IpfsClient, TryFromUri};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct IPFSFile {
+    ethereum_public_key: String,
+    ethereum_address: String,
+    aptos_public_key: String,
+    aptos_address: String,
+    message: String,
+    ethereum_signature: String,
+    aptos_signature: String,
+}
 
 pub fn verify_signature_by_public_key_ethereum(
     ethereum_signature: &str,
@@ -41,15 +62,43 @@ pub fn verify_signature_by_public_key_aptos(
     result.is_ok()
 }
 
-pub async fn upload_ipfs(data: String) -> String {
-    panic!("Not implemented yet")
+pub async fn upload_ipfs(data: String) -> Result<String, String> {
+    let IPFS_API_KEY = env::var("IPFS_API_KEY").expect("IPFS_API_KEY must be set");
+    let IPFS_API_KEY_SECRET = env::var("IPFS_API_KEY_SECRET").expect("IPFS_API_KEY_SECRET must be set");
+
+    let client = reqwest::Client::new();
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(
+        "Authorization",
+        format!("Basic {}", base64::encode(format!("{}:{}", IPFS_API_KEY, IPFS_API_KEY_SECRET))).parse().unwrap(),
+    );
+
+    let form: reqwest::multipart::Form = reqwest::multipart::Form::new()
+        .part("file", reqwest::multipart::Part::bytes(data.as_bytes().to_vec()).file_name("ipfs_upload.json"));
+
+    let mut response = client
+        .post("https://ipfs.infura.io:5001/api/v0/add")
+        .headers(headers)
+        .multipart(form)
+        .send()
+        .await
+        .map_err(|e| {
+            println!("Response Error: {:?}", e);
+            e.to_string()
+        })?;
+
+    let text = response.text().await.map_err(|e| e.to_string())?;
+    let json: serde_json::Value = serde_json::from_str(&text).unwrap();
+    let hash = json["Hash"].as_str().unwrap();
+    println!("hash: {}", hash);
+    Ok(hash.to_string())
 }
 
 #[cfg(test)]
 mod tests {
     use std::env;
     use crate::package::aptos::tests::AccountSignature::Ed25519Signature;
-    use crate::package::aptos::verify_signature_by_public_key_ethereum;
+    use crate::package::aptos::{upload_ipfs, verify_signature_by_public_key_ethereum};
     use aptos_sdk::crypto::ed25519::Ed25519Signature as Ed25519SignatureTuple;
     use aptos_sdk::crypto::ed25519::{Ed25519PrivateKey, Ed25519PublicKey};
     use aptos_sdk::crypto::{
@@ -66,6 +115,7 @@ mod tests {
     use rand::rngs::StdRng;
     use rand::SeedableRng;
     use std::str::FromStr;
+    use ipfs_api_backend_actix::{IpfsApi, IpfsClient};
 
     #[test]
     fn sign_message_by_aptos_public_key() {
@@ -167,5 +217,25 @@ mod tests {
             &signature_str,
             "0x01725BE700413D34bCC5e961de1d0C777d3A52F4"
         ));
+    }
+
+    #[actix_web::test]
+    async fn test_upload_ipfs() {
+        let data = r#"{
+        "ethereum_public_key": "none",
+        "ethereum_address": "0x01725BE700413D34bCC5e961de1d0C777d3A52F4",
+        "aptos_public_key": "0x917e745db1b1edc9ce62a8ed62b1cfcf261b4a5a21d58b167874e6cf6fa68aa3",
+        "aptos_address": "0x470196fa19f82ece3bfe1f4658c12098f752c5ae01a43a4f57cb46fbf05c1011",
+        "message": "aptosgazua",
+        "ethereum_signature": "71bcbc3edb59e4182c7d8dddf38df23d5220194fbe0e6f4577d4137c91c678a00825785fee253b93944c05202e339109e295cc523818995e5307813a8282e3cc1b",
+        "aptos_signature": "35b6acdafabae15db4083b60bd801bd7e532c5fd038844d592b0b677f67b6dfb12ed2a810ea107c23c27e6ecce300cf9233c3efe5c8fb59248ee2aafc9be7a00"
+    }"#.to_string();
+
+        let hash = upload_ipfs(data.clone()).await;
+        assert!(hash.is_ok());
+        assert!(!hash.unwrap().as_str().is_empty());
+
+        // let downloaded_data = download_ipfs(hash.unwrap().as_str()).await;
+        // assert!(downloaded_data.is_ok());
     }
 }
